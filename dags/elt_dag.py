4e27965345d1778e0d0b630d205b717a79c8c36c
LeventSoykan <levent_soykan@yahoo.com>
2024-01-03 11:55:40 +0300

    Create a sample dag with Python and Bashoperator

---
+++ dags/elt_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'your_dag',
+    default_args=default_args,
+    description='Run Python script and dbt model weekly',
+    schedule_interval=timedelta(weeks=1),
+)
+
+# Python script task
+run_python_script = PythonOperator(
+    task_id='run_python_script',
+    python_callable=lambda: print("Running your_python_script.py"),
+    dag=dag,
+)
+
+# dbt run task
+run_dbt = BashOperator(
+    task_id='run_dbt',
+    bash_command='docker run --rm -v $(pwd):/dbt_project your_dbt_image_name',
+    dag=dag,
+)
+
+# Set the task order
+run_python_script >> run_dbt
