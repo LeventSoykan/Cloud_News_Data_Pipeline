00d64a766d4cd801bcfa6bd96a67f98d6dbc9fba
LeventSoykan <levent_soykan@yahoo.com>
2024-01-04 13:49:35 +0300

    Update DBT to create a table io view

--- dbt/cloudnews/models/cloudnews/articles.sql
+++ dbt/cloudnews/models/cloudnews/articles.sql
@@ -1,11 +1,10 @@
 WITH transformed_data AS (
-  SELECT
-    title, url, source, date::DATE
-  FROM raw
+      SELECT
+        title, url, source, date::DATE, query_date::DATE
+      FROM raw
+    )
 
-)
-
-SELECT
-  *
-FROM
-  transformed_data
+    SELECT
+      *
+    FROM
+      transformed_data
